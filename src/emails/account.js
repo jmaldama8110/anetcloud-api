c04449b14c3d99e6322597896d7f185f14de7f23
Josman <josemanuek@hotmail.com>
2025-02-18 10:11:16 -0600

    force deploy

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,49 +5,6 @@ const axios = require("axios");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
-const sendConfirmationEmail = ( email, name, code )=>{
-
-    sgMail.send({
-        to: email,
-        from: 'ANET Cloud<jm@aristanetworks.cloud>',
-        subject: `${name} Confirm your email!`,
-        text: ` In order to confirm your email, please provide the following code: ${code}`
-    })
-
-}
-
-const sendWelcomeEmail = ( email, name )=>{
-
-    sgMail.send({
-        to: email,
-        from: 'ANET Cloud<jm@aristanetworks.cloud>',
-        subject: `${name} Welcome to ANet Cloud!`,
-        text: `Thank you, your email has been confirmed!`
-    })
-
-}
-const sendPaymentLinkEmail = ( email, suscriptionId, paymentUrl )=>{
-
-    sgMail.send({
-        to: email,
-        from: 'ANET Cloud<jm@aristanetworks.cloud>',
-        subject: `A payment link for ${suscriptionId} is waiting to be processed `,
-        text: `Please click here to provide your payment ${paymentUrl}`
-    })
-
-}
-
-const sendGoodbyEmail = (email, name) =>{
-
-    sgMail.send({
-        to: email,
-        from: 'ANET Cloud<jm@aristanetworks.cloud>',
-        subject: `${name}Sorry to see you go! hopefully we come back soon`,
-        text: ` Hi ${name} we are going to miss you!, if theres is something we can do to help you please let us know... We'd loved to get some feedback from you! Hope to see you soon!`
-    })
-
-}
-
 async function sendTemplateEmail (data, template_id ) {
 
     const sendGridApiKey = process.env.SENDGRID_API_KEY ? process.env.SENDGRID_API_KEY : "";
